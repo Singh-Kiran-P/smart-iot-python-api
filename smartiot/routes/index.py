36f3d28f896791cade4ad14298caed0f6347194f
Singh Kiran <singh.kiran2456@hotmail.com>
2019-01-22 00:19:07 +0100

    return smartiot apk

--- smartiot/routes/index.py
+++ smartiot/routes/index.py
@@ -1,6 +1,6 @@
 from smartiot import app
 from smartiot.bin.config.db_config import mysql
-from flask import Flask,render_template,flash,redirect,session,url_for,logging,request,Blueprint,json
+from flask import Flask,render_template,flash,redirect,session,url_for,logging,request,Blueprint,json,send_file
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from flask_mail import Mail, Message
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired
@@ -14,3 +14,10 @@ index_bp = Blueprint(
 @index_bp.route("/")
 def index():
     return render_template('home.html')
+
+@index_bp.route("/download/smartiotapk")
+def downloadAPK():
+    try:    
+        return send_file('routes/downloads/smartiot.apk', attachment_filename='smart_0.1.apk')
+    except Exception as e:
+        return str(e)
